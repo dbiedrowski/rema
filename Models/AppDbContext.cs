1bfb2ebc9ce5964fc679ad8ae32f67ab62f57d9c
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-08 15:41:52 +0100

    REMA-5: Add custom DbContext

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Landlord> Landlords { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
