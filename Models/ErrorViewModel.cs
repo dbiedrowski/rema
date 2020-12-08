4ac3a6b7ac5551eccfa4cd0d70c51b61c524a347
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-08 13:58:59 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace REMA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
