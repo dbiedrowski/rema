afdc5bf212712253556777df856d265815aafcea
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-08 14:03:15 +0100

    REMA-2: finish the task

---
+++ Models/Landlord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Models
+{
+    public class Landlord
+    {
+        public int Id { get; set; }
+        public Profile Profile { get; set; }
+    }
+}
