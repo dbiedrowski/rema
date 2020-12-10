02e2f0e2a27bbe37e4dfa7db81936d7568006908
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-10 12:55:51 +0100

    REMA-13: Move repository interfaces to Data folder

--- Data/ILandlordRepository.cs
+++ Data/ILandlordRepository.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using REMA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace REMA.Models
+namespace REMA.Data
 {
     public interface ILandlordRepository
     {  
---
+++ Data/IProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Data
+{
+    public interface IProfileRepository
+    {  
+        public Profile CreateProfile(Profile profile);
+        public Profile GetProfile(int id);
+        IEnumerable<Profile> AllProfiles { get; }
+        public Profile UpdateProfile(Profile profile);
+        public Profile DeleteProfile(Profile profile);
+    }
+}
