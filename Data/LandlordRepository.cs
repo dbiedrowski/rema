10b75824a7a8d9576e83997fb2fcc50cc7111536
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-10 13:53:53 +0100

    REMA-14: finish the task

---
+++ Data/LandlordRepository.cs
@@ -0,0 +1,62 @@
+﻿using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Data
+{
+    public class LandlordRepository : ILandlordRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LandlordRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Landlord> AllLandlords
+        {
+            get
+            {
+                return _context.Landlords.AsEnumerable<Landlord>();
+            }
+        }
+
+        public Landlord CreateLandlord(Landlord landlord)
+        {
+            _context.Landlords.Add(landlord);
+            _context.SaveChanges();
+
+            return landlord;
+        }
+
+        public Landlord DeleteLandlord(int id)
+        {
+            var landlord = GetLandlord(id);
+
+            if(landlord != null)
+            {
+                _context.Landlords.Remove(landlord);
+                _context.SaveChanges();
+            }
+
+            return landlord;
+        }
+
+        public Landlord GetLandlord(int id)
+        {
+            return _context.Landlords.FirstOrDefault(llord => llord.Id == id);
+        }
+
+        public Landlord UpdateLandlord(Landlord landlord)
+        {
+            if (_context.Landlords.Find(landlord.Id) != null)
+            {
+                _context.Landlords.Update(landlord);
+                _context.SaveChanges();
+            }
+
+            return landlord;
+        }
+    }
+}
---
+++ Data/ProfileRepository.cs
@@ -0,0 +1,62 @@
+﻿using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Data
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProfileRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Profile> AllProfiles
+        {
+            get
+            {
+                return _context.Profiles.AsEnumerable<Profile>();
+            }
+        }
+
+        public Profile CreateProfile(Profile profile)
+        {
+            _context.Profiles.Add(profile);
+            _context.SaveChanges();
+
+            return profile;
+        }
+
+        public Profile DeleteProfile(int id)
+        {
+            var profile = GetProfile(id);
+
+            if(profile != null)
+            {
+                _context.Profiles.Remove(profile);
+                _context.SaveChanges();
+            }
+
+            return profile;
+        }
+
+        public Profile GetProfile(int id)
+        {
+            return _context.Profiles.FirstOrDefault(llord => llord.Id == id);
+        }
+
+        public Profile UpdateProfile(Profile profile)
+        {
+            if (_context.Profiles.Find(profile.Id) != null)
+            {
+                _context.Profiles.Update(profile);
+                _context.SaveChanges();
+            }
+
+            return profile;
+        }
+    }
+}
