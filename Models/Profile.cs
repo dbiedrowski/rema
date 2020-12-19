108d4f2bb3e89853d23c0b44d99dcefffa7035e5
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-19 19:23:15 +0100

    REMA-39: Remove the fields

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -10,7 +10,5 @@ namespace REMA.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
     }
 }
