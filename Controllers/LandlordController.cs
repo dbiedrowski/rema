b43cf70cf8279c59d46a14d5892267c5c7277760
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-19 18:34:08 +0100

    REMA-34: finish the task

--- Controllers/LandlordController.cs
+++ Controllers/LandlordController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using REMA.Data;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace REMA.Controllers
 {
+    [Authorize]
     public class LandlordController : Controller
     {
         private readonly ILandlordRepository landlordRepository;
