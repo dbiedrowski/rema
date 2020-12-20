6e25d9b957056e8cd35b135303c7217031e65466
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-20 16:17:47 +0100

    REMA-44: Make UserId string for Landlord

---
+++ Migrations/20201220151657_MakeUserIdStringForLandlord.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace REMA.Migrations
+{
+    public partial class MakeUserIdStringForLandlord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Landlords",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Landlords",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Landlords",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UserId",
+                value: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Landlords",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Landlords",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Landlords",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UserId",
+                value: 0);
+        }
+    }
+}
--- Models/Landlord.cs
+++ Models/Landlord.cs
@@ -11,7 +11,7 @@ namespace REMA.Models
     {
         public int Id { get; set; }
         public int ProfileId { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public Profile Profile { get; set; }
     }
 }
