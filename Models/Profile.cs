4dc7c3f1bb2d6763a6b9f6b7fb046db3c3d7d91a
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-20 12:45:17 +0100

    REMA-41: Add EF attributes

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace REMA.Models
 {
     public class Profile
     {
+        [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
