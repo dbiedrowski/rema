9d83087289b35879eaf717403374b27ed2e60c0f
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-26 15:02:34 +0100

    REMA-22: interface plus empty implementation

---
+++ Data/ApartmentRepository.cs
@@ -0,0 +1,37 @@
+﻿using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Data
+{
+    public class ApartmentRepository : IApartmentRepository
+    {
+
+        public ApartmentRepository Create(ApartmentRepository apartment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApartmentRepository Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApartmentRepository GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApartmentRepository GetByLandlord(Landlord landlord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApartmentRepository Update(ApartmentRepository apartment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/IApartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Data
+{
+    interface IApartmentRepository
+    {
+        public ApartmentRepository Create(ApartmentRepository apartment);
+        public ApartmentRepository GetById(int id);
+        public ApartmentRepository GetByLandlord(Landlord landlord);
+        public ApartmentRepository Update(ApartmentRepository apartment);
+        public ApartmentRepository Delete(int id);
+    }
+}
