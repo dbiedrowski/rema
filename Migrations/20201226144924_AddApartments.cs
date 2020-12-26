d8a642d8b1b240657555dff641aac1f9b87080ed
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-26 16:14:18 +0100

    REMA-49: Finish the task

---
+++ Migrations/20201226144924_AddApartments.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace REMA.Migrations
+{
+    public partial class AddApartments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Landlords",
+                newName: "LandlordId");
+
+            migrationBuilder.CreateTable(
+                name: "Apartments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Floor = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuildingFloors = table.Column<int>(type: "int", nullable: false),
+                    AvailableSince = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    LandlordId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apartments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Apartments_Landlords_LandlordId",
+                        column: x => x.LandlordId,
+                        principalTable: "Landlords",
+                        principalColumn: "LandlordId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apartments_LandlordId",
+                table: "Apartments",
+                column: "LandlordId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apartments");
+
+            migrationBuilder.RenameColumn(
+                name: "LandlordId",
+                table: "Landlords",
+                newName: "Id");
+        }
+    }
+}
