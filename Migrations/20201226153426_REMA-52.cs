e0b52326e66c16850b911b18e55b26216c11de21
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-26 16:35:05 +0100

    REMA-52: Change in db

---
+++ Migrations/20201226153426_REMA-52.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace REMA.Migrations
+{
+    public partial class REMA52 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Apartments",
+                newName: "ApartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ApartmentId",
+                table: "Apartments",
+                newName: "Id");
+        }
+    }
+}
