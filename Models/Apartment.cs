37cfd8365c5092141265017cc1135c2059522723
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-26 16:33:52 +0100

    REMA-52: Change in code

--- Models/Apartment.cs
+++ Models/Apartment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace REMA.Models
 {
     public class Apartment
     {
-        public int Id { get; set; }
+        [Key]
+        public int ApartmentId { get; set; }
         public string Address { get; set; }
         public int Floor { get; set; }
         public string Type { get; set; }
