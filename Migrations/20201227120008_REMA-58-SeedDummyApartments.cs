9ffb01e2477fe64b08c8dc4c21a35d34648a4339
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-27 14:44:23 +0100

    REMA-58: add migration

---
+++ Migrations/20201227120008_REMA-58-SeedDummyApartments.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace REMA.Migrations
+{
+    public partial class REMA58SeedDummyApartments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apartments_Addresses_AddressId",
+                table: "Apartments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apartments_Landlords_LandlordId",
+                table: "Apartments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LandlordId",
+                table: "Apartments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Apartments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "AddressId", "City", "Country", "State", "StreetName", "StreetNumber", "ZipCode" },
+                values: new object[] { 1, "Łódź", null, null, "Piękna", "1/1", "93-123" });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "AddressId", "City", "Country", "State", "StreetName", "StreetNumber", "ZipCode" },
+                values: new object[] { 2, "Łódź", null, null, "Bestii", "2/12", "93-234" });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "AddressId", "City", "Country", "State", "StreetName", "StreetNumber", "ZipCode" },
+                values: new object[] { 3, "Łódź", null, null, "Waleczna", "15/28", "93-345" });
+
+            migrationBuilder.InsertData(
+                table: "Apartments",
+                columns: new[] { "ApartmentId", "AddressId", "Area", "AvailableSince", "BuildingFloors", "Floor", "LandlordId", "Type" },
+                values: new object[] { 1, 1, 50.390000000000001, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, 2, 1, 0 });
+
+            migrationBuilder.InsertData(
+                table: "Apartments",
+                columns: new[] { "ApartmentId", "AddressId", "Area", "AvailableSince", "BuildingFloors", "Floor", "LandlordId", "Type" },
+                values: new object[] { 2, 2, 48.119999999999997, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, 0, 1, 0 });
+
+            migrationBuilder.InsertData(
+                table: "Apartments",
+                columns: new[] { "ApartmentId", "AddressId", "Area", "AvailableSince", "BuildingFloors", "Floor", "LandlordId", "Type" },
+                values: new object[] { 3, 3, 56.219999999999999, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, 1, 2, 0 });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apartments_Addresses_AddressId",
+                table: "Apartments",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "AddressId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apartments_Landlords_LandlordId",
+                table: "Apartments",
+                column: "LandlordId",
+                principalTable: "Landlords",
+                principalColumn: "LandlordId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apartments_Addresses_AddressId",
+                table: "Apartments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apartments_Landlords_LandlordId",
+                table: "Apartments");
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "ApartmentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "ApartmentId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "ApartmentId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Addresses",
+                keyColumn: "AddressId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Addresses",
+                keyColumn: "AddressId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Addresses",
+                keyColumn: "AddressId",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LandlordId",
+                table: "Apartments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Apartments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apartments_Addresses_AddressId",
+                table: "Apartments",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "AddressId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apartments_Landlords_LandlordId",
+                table: "Apartments",
+                column: "LandlordId",
+                principalTable: "Landlords",
+                principalColumn: "LandlordId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
