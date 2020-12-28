2ebeb758327543380d011b68b00d4ea48515fd1d
Damian Biedrowski <damian.biedrowski@gmail.com>
2020-12-28 16:59:18 +0100

    REMA-61: Add view and action methods

--- Controllers/ApartmentController.cs
+++ Controllers/ApartmentController.cs
@@ -17,6 +17,7 @@ namespace REMA.Controllers
         private readonly ILandlordRepository _landlordRepository;
         private readonly IApartmentRepository _apartmentRepository;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly Landlord _landlord;
 
         public ApartmentController(ILandlordRepository landlordReporitory,
                                     IApartmentRepository apartmentRepository,
@@ -25,14 +26,35 @@ namespace REMA.Controllers
             _landlordRepository = landlordReporitory;
             _apartmentRepository = apartmentRepository;
             _httpContextAccessor = httpContextAccessor;
+
+            var userId = _httpContextAccessor.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier).Value;
+            _landlord = _landlordRepository.GetLandlord(userId);
         }
 
         public IActionResult Index()
         {
-            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            Landlord landlord = _landlordRepository.GetLandlord(userId);
-            IEnumerable<Apartment> apartments = _apartmentRepository.GetByLandlord(landlord);
+            IEnumerable<Apartment> apartments = _apartmentRepository.GetByLandlord(_landlord);
             return View(apartments);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Apartment apartment = new Apartment();
+
+            return View(apartment);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Apartment apartment)
+        {
+            apartment.Landlord = _landlord;
+            apartment.LandlordId = _landlord.LandlordId;
+
+            _apartmentRepository.Create(apartment);
+
+            return RedirectToAction("Index");
         }
     }
 }
