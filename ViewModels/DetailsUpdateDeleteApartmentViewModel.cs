a874fd5c64f171310229d2bf24ea67ae615d0db0
Damian Biedrowski <damian.biedrowski@gmail.com>
2021-01-03 11:13:11 +0100

    REMA-73: Add the view model

---
+++ ViewModels/DetailsUpdateDeleteApartmentViewModel.cs
@@ -0,0 +1,85 @@
+﻿using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.ViewModels
+{
+    public class DetailsUpdateDeleteApartmentViewModel
+    {
+        // Apartment address information
+        public int AddressId { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        // The rest of apartment information
+        public int ApartmentId { get; set; }
+        public int Floor { get; set; }
+        public BuildingType BuildingType { get; set; }
+        public int BuildingFloors { get; set; }
+        public DateTime AvailableSince { get; set; }
+        public double Area { get; set; }
+
+        internal Apartment ToDomainModel()
+        {
+            return new Apartment()
+            {
+                Address = new Address
+                {
+                    StreetName = StreetName,
+                    StreetNumber = StreetNumber,
+                    City = City,
+                    ZipCode = ZipCode,
+                    State = State,
+                    Country = Country
+                },
+                Floor = Floor,
+                BuildingType = BuildingType,
+                BuildingFloors = BuildingFloors,
+                AvailableSince = AvailableSince,
+                Area = Area
+            };
+        }
+
+        internal static IEnumerable<DetailsUpdateDeleteApartmentViewModel> ToViewModels(
+            IEnumerable<Apartment> apartments)
+        {
+            List<DetailsUpdateDeleteApartmentViewModel> viewModels
+                = new List<DetailsUpdateDeleteApartmentViewModel>();
+            foreach (var apartment in apartments)
+            {
+                viewModels.Add(DetailsUpdateDeleteApartmentViewModel.ToViewModel(apartment));
+            }
+
+            return viewModels.AsEnumerable();
+        }
+
+        internal static DetailsUpdateDeleteApartmentViewModel ToViewModel(Apartment apartment)
+        {
+            DetailsUpdateDeleteApartmentViewModel apartmentViewModel = new DetailsUpdateDeleteApartmentViewModel()
+            {
+                AddressId = apartment.Address.AddressId,
+                StreetName = apartment.Address.StreetName,
+                StreetNumber = apartment.Address.StreetNumber,
+                City = apartment.Address.City,
+                ZipCode = apartment.Address.ZipCode,
+                State = apartment.Address.State,
+                Country = apartment.Address.Country,
+
+                ApartmentId = apartment.ApartmentId,
+                Floor = apartment.Floor,
+                BuildingType = apartment.BuildingType,
+                BuildingFloors = apartment.BuildingFloors,
+                AvailableSince = apartment.AvailableSince,
+                Area = apartment.Area
+            };
+
+            return apartmentViewModel;
+        }
+    }
+}
