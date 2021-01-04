868fedece1a9839afded067ed414f06e8b5b44a1
Damian Biedrowski <damian.biedrowski@gmail.com>
2021-01-04 12:14:52 +0100

    REMA-77: Add Room model

---
+++ Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using REMA.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public decimal Price { get; set; }
+        public RoomType RoomType { get; set; }
+        public double Area { get; set; }
+        public bool IsFurnished { get; set; }
+        public Apartment Apartment { get; set; }
+        public int ApartmentId { get; set; }
+    }
+}
