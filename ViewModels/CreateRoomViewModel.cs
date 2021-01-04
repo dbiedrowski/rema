13a68ffac2a7de99ec6fc6ac2c7542dc09a89f50
Damian Biedrowski <damian.biedrowski@gmail.com>
2021-01-04 12:26:41 +0100

    REmA-79: Create CreateRoomViewModel

---
+++ ViewModels/CreateRoomViewModel.cs
@@ -0,0 +1,17 @@
+﻿using REMA.Enums;
+using REMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.ViewModels
+{
+    public class CreateRoomViewModel
+    {
+        public decimal Price { get; set; }
+        public RoomType RoomType { get; set; }
+        public double Area { get; set; }
+        public bool IsFurnished { get; set; }
+    }
+}
