7f757b63c764fbd6fe849aa5f6a2f2bc21071fb5
Damian Biedrowski <damian.biedrowski@gmail.com>
2021-01-05 13:12:17 +0100

    REMA-85: Add room controller action

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using REMA.Data;
+using REMA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMA.Controllers
+{
+    [Authorize]
+    public class RoomController : Controller
+    {
+        private readonly IRoomRepository _roomRepository;
+
+        public RoomController(IRoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Add(int apartmentId)
+        {
+            CreateRoomViewModel viewModel = new CreateRoomViewModel();
+            viewModel.ApartmentId = apartmentId;
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreateRoomViewModel viewModel)
+        {
+            _roomRepository.Create(viewModel.ToDomainModel());
+            return RedirectToAction("Details", "Apartment", new { apartmentId = viewModel.ApartmentId });
+        }
+    }
+}
--- ViewModels/CreateRoomViewModel.cs
+++ ViewModels/CreateRoomViewModel.cs
@@ -13,5 +13,18 @@ namespace REMA.ViewModels
         public RoomType RoomType { get; set; }
         public double Area { get; set; }
         public bool IsFurnished { get; set; }
+        public int ApartmentId { get; set; }
+
+        internal Room ToDomainModel()
+        {
+            return new Room()
+            {
+                ApartmentId = ApartmentId,
+                Area = Area,
+                IsFurnished = IsFurnished,
+                Price = Price,
+                RoomType = RoomType
+            };
+        }
     }
 }
